d80c2d58e0aab6c6ed2877b07ec5cf59098c68f5
Kashif Husain <kashif.husain@hpe.com>
2017-09-28 12:11:33 +0100

    defect #1663:

--- shipex/src/test/java/ShippingExpress/ShipexApplicationTests.java
+++ shipex/src/test/java/ShippingExpress/ShipexApplicationTests.java
@@ -128,7 +128,7 @@ public class ShipexApplicationTests {
         //change
         //modify code xxx uuuugg
         // change some codeeee
-        //changesee
+        //change
 
 // change code
      
